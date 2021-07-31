03e41f14d9aeb62e2235145f8d40329770c4a547
José Padron <padroncjosel@gmail.com>
2021-07-31 15:09:08 -0400

    fix: fixed progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,6 +1,6 @@
 export default function ProgressBar() {
   return (
-    <div className='h-1 absolute w-full bg-white z-50 animate-pulse'>
+    <div className='h-1 fixed top-0 w-full bg-white z-50 animate-pulse'>
       <div className='bg-primary w-full h-full'></div>
     </div>
   )
